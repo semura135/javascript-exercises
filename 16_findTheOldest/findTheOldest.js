3b1ffa73a31ca8c8b0df01fe02c43b356b245ae7
semura135 <semirabereka135@gmail.com>
2025-07-26 17:20:53 +0300

    Finished Javascript exercises(12, 13, 14, 15, 16)

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+  
+  person.yearOfDeath ??= new Date().getFullYear();
 
+  return person.yearOfDeath - person.yearOfBirth;
+};
+
+const findTheOldest = function (people) {
+  const peopleOldestToYoungest = people.toSorted(
+    (person, nextPerson) => getAge(nextPerson) - getAge(person),
+  );
+
+  const oldestPerson = peopleOldestToYoungest[0];
+  return oldestPerson;
 };
 
 // Do not edit below this line
